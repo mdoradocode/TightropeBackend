cd8ca406f6dfb099660b5314297dd09a93920f47
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2022-02-10 13:10:54 -0800

    GET, POST, and PUT updated to work

--- eventApp/urls.py
+++ eventApp/urls.py
@@ -5,7 +5,6 @@ from eventApp import views
 urlpatterns=[
     #Route requests with the event tag on them to the events api
     path(r'events/',views.eventsAPI),
-    #if the request has an id attached (it will) also route it to the API
-    #path(r'events/<str:username>',views.eventsAPI)
-
+    #if the request has an UserEmail attached (it will) also route it to the API
+    path(r'events/<str:useremail>',views.eventsAPI)
 ]
